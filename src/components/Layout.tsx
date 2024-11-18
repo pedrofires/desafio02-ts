4f17941b845540faec5bc5def8ce0153f9feefc6
Pedro Fires <pedrof.fires@gmail.com>
2024-11-17 21:16:12 -0300

    feat: added application to the new layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,20 @@
-import { Footer } from "./Footer"
-import { Header } from "./Header/Header"
+import React from "react";
+import { Footer } from "./Footer/Footer";
+import { Header } from "./Header/Header";
 
 export const Layout = ({ children }: any) => {
-  return(
-    <>
+  return (
+    <body
+      style={{
+        minHeight: "100dvh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+    >
       <Header />
-      { children }
+      {children}
       <Footer />
-    </>
-  )
-}
+    </body>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Layout } from "./components/Layout";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Layout>
+      <App />
+    </Layout>
   </React.StrictMode>
 );
 
